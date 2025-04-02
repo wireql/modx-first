1f0960976d46ae827a43971f97715249964295d9
wireql <dmtr.off.p@gmail.com>
2025-04-02 20:47:45 +0300

    сверстал блоки и добавил слайдер

--- src/main.js
+++ src/main.js
@@ -1,1 +1,16 @@
+import Swiper from "swiper";
+import { Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
 import "./style.css";
+
+const swiper = new Swiper(".swiper", {
+    modules: [Pagination],
+    direction: "horizontal",
+    slidesPerView: 1.2,
+    spaceBetween: 20,
+
+    pagination: {
+        el: ".swiper-pagination",
+    },
+});
